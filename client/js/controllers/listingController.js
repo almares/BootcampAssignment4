81990d3025fab33a09aa8fbb384347e6db278f47
almares <42773270+almares@users.noreply.github.com>
2018-09-25 21:47:56 -0400

    Update listingController.js

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,26 +1,23 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     /* Get all the listings, then bind it to the scope */
     Listings.getAll().then(function(response) {
       $scope.listings = response.data;
     }, function(error) {
-      console.log('Unable to retrieve listings:', error);
+      console.log('Listings could not be retrieved: ', error);
     });
 
     $scope.detailedInfo = undefined;
 
     $scope.addListing = function() {
-	  /**TODO 
-	  *Save the article using the Listings factory. If the object is successfully 
-	  saved redirect back to the list page. Otherwise, display the error
-	 */
+      Listings.create($scope.newListing);
+      location.reload();
     };
 
     $scope.deleteListing = function(index) {
-	   /**TODO
-        Delete the article using the Listings factory. If the removal is successful, 
-		navigate back to 'listing.list'. Otherwise, display the error. 
-       */
+      var list = $scope.listings[index];
+      Listings.delete(listings._id);
+      location.reload();
     };
 
     $scope.showDetails = function(index) {
